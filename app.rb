2d487762eaafa399b8348e2796f66189b80cd64f
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-01-27 18:23:02 -0500

    commit tree 983d0ef363c8724f172e0df5bb4cf73e326bddf2

---
+++ app.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+#puts "Hello world!"
+#puts "Hello world!"
+#puts "Hello world!"
+
+
+print "Pass this test, please."
+#print "Hello world!"
+#print "Hello world!"
+#print "Hello world!"
+
+puts [1, 2, 3]
+
+p [1, 2, 3]
+
+#pp [1, 2, 3]
